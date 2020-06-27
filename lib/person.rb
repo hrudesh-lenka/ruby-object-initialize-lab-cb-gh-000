38917ddcad643508a4133439fa3f2069b6c3831d
Hrudesh Lenka <hrudesh2010@gmail.com>
2020-06-27 07:33:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,4 @@
-class Person
-  attr_accessor :name, :job
-  def name
-    @name
-  end
-  def name=(name)
+class Dog
+  def initialize(name)
     @name = name
   end
-  def job
-    @job
-  end
-  def job=(job)
-    @job = job
-  end
-end
