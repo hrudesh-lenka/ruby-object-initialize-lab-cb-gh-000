60365a143b61b36f8b849ebd389c57aaaaef3ae0
Hrudesh Lenka <hrudesh2010@gmail.com>
2020-06-27 07:39:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,16 @@ class Dog
     @name = name
     @breed = breed
   end
+  def name
+    @name
+  end
+  def name=(name)
+    @name = name
+  end
+  def breed
+    @breed
+  end
+  def breed=(breed)
+    @breed = breed
+  end
 end
